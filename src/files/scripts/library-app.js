dce24afb9a2aef4d307051a01c8fafd636bf2ab4
risent <shengqi542@gmail.com>
2015-05-22 16:44:25 +0800

    Add color list support.

--- src/files/scripts/library-app.js
+++ src/files/scripts/library-app.js
@@ -19,6 +19,9 @@ App.models.Category = Backbone.Model.extend({
 	urlRoot: API_ROOT + 'templatecategory'
 })
 
+App.models.Klass = Backbone.Model.extend({
+	urlRoot: API_ROOT + 'templateclass'
+})
 
 
 App.collections.CaseCollection = Backbone.Collection.extend({
@@ -48,6 +51,41 @@ App.collections.CategoryCollection = Backbone.Collection.extend({
 	}
 })
 
+App.collections.KlassCollection = Backbone.Collection.extend({
+	// model: App.models.Case,
+	url: API_ROOT + 'templateklass',
+	model: App.models.Klass,
+	parse: function(response) {
+		return response.objects;
+	}
+})
+
+
+
+App.views.KlassView = Backbone.View.extend({
+	el: '#klass-wrap',
+	events: {
+		"click": "open"
+	},
+
+	initialize: function() {
+		this.isLoading = false;
+		this.klassCollection = new App.collections.KlassCollection();
+	},
+
+	render: function() {
+		var that = this;
+		this.klassCollection.fetch({success: function(klassList) {
+			var klassList = that.categoryCollection.models;
+			var template = _.template($('#klass-list-tpl').html());
+			var klassHtml = template({klassList: klassList, _:_});
+			$(that.$el).append($(klassHtml));
+		}});
+		return this;
+	}
+
+})
+
 
 App.views.ColorView = Backbone.View.extend({
 	el: '#color-wrap',
@@ -63,6 +101,7 @@ App.views.ColorView = Backbone.View.extend({
 	render: function() {
 		var that = this;
 		this.colorCollection.fetch({success: function(colorList) {
+			var colorList = that.colorCollection.models;
 			var template = _.template($('#color-list-tpl').html());
 			var colorHtml = template({colorList: colorList, _:_});
 			$(that.$el).append($(colorHtml));
@@ -72,6 +111,29 @@ App.views.ColorView = Backbone.View.extend({
 
 })
 
+App.views.CategoryView = Backbone.View.extend({
+	el: '#category-wrap',
+	events: {
+		"click": "open"
+	},
+
+	initialize: function() {
+		this.isLoading = false;
+		this.categoryCollection = new App.collections.CategoryCollection();
+	},
+
+	render: function() {
+		var that = this;
+		this.categoryCollection.fetch({success: function(categoryList) {
+			var categoryList = that.categoryCollection.models;
+			var template = _.template($('#category-list-tpl').html());
+			var categoryHtml = template({categoryList: categoryList, _:_});
+			$(that.$el).append($(categoryHtml));
+		}});
+		return this;
+	}
+
+})
 
 App.views.CaseListView = Backbone.View.extend({
 	el: '#item-wrap',
@@ -113,6 +175,9 @@ App.views.CaseListView = Backbone.View.extend({
 
 
 var colorView = new App.views.ColorView();
+var categoryView = new App.views.CategoryView();
+
 var appView = new App.views.CaseListView();
-// colorView.render();
+colorView.render();
+categoryView.render();
 appView.render();
