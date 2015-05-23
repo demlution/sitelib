be443f83fdfbd7c743acbe5856695e103a5b5f0b
chenj <chenj@demlution.com>
2015-05-23 08:55:10 +0800

    add search-bar

--- src/files/scripts/scripts.js
+++ src/files/scripts/scripts.js
@@ -1,1 +1,7 @@
 console.log('hi');
+$(".s-left .s-txt").focus(function(){
+				$(this).animate({width:"150px"});
+			});
+			$(".s-left .s-txt").blur(function(){
+				$(this).animate({width:"80px"});
+			});
