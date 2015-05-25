cc86da8aaf96f7fd636558491c0e88c2918275ee
chenj <chenj@demlution.com>
2015-05-25 23:11:45 +0800

    update index

--- src/files/scripts/scripts.js
+++ src/files/scripts/scripts.js
@@ -19,4 +19,4 @@ $(function() {
 })
 
 
-$("#solution-info").actabctl({menu:'.tit', content: '.tab-pane',selectedCss: "active", evt:1});
+$("#solution-info").actabctl({menu:'.tit', content: '.tab-pane',selectedCss: "active", evt:0});
