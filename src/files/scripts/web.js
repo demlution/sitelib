fc4bbad2d007bbedd22ecb3b35b36b99cb114c5e
chenj <chenj@demlution.com>
2015-05-26 11:09:19 +0800

    add 400tel

---
+++ src/files/scripts/web.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	
+	$('.youshilist > li').each(function(i){						
+		$(this).hover(function(){
+			$('.yshover').stop().animate({left: i*150},300);
+			$('.ystextlist').stop().animate({top:i*-85+10},200);
+		});						
+	});
+	
+	$('.youshilist > li').hover(function(){	
+		$(this).css({color:'#4093d8',cursor:'pointer'})
+			},function(){
+		$(this).css({color:''})
+	});
+
+	$('.youshtext > p').each(function(i){
+		$(this).css({top: i*50+15});								  
+	});
+	
+});
